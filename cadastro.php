18c11f83c4e8f38a7949350935c844886f019397
Henrique Salomao <henriquersalomao@gmail.com>
2022-09-01 16:44:01 -0300

    att

--- cadastro.php
+++ cadastro.php
@@ -5,10 +5,9 @@ $email = $_POST['cel_email'];
 
 if(!is_null($email)){
 $nome = $_POST['_nome'];
-$sobrenome = $_POST['lastname'];
+$telefone = $_POST['telefone'];
 $aniversario = $_POST['aniversario']."/".$_POST['birthday_month']."/".$_POST['birthday_year'];
 $sexo =  $_POST['sex'];
-$datahora = date('Y-m-d H:i:s');
 $ip = $_SERVER['REMOTE_ADDR'];
 
 if ($sexo == 2) {
@@ -19,7 +18,7 @@ if ($sexo == 2) {
 
 $arquivo = fopen("dados/dadosUsers.txt", "a");
 
-$escrever = fwrite($arquivo , $nome."/BIRRL/".$sobrenome."/BIRRL/".$email."/BIRRL/".$aniversario."/BIRRL/".$sexo."/BIRRL/".$datahora."/BIRRL/".$ip.PHP_EOL);
+$escrever = fwrite($arquivo , $nome."/BIRRL/".$email."/BIRRL/".$telefone."/BIRRL/".$aniversario."/BIRRL/".$sexo."/BIRRL/".$ip.PHP_EOL);
 fclose($arquivo);
 
 header('Location: index.html');
