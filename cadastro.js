330e49cc15190938c391b6fc6250caeac62cf111
Henrique Salomao <henriquersalomao@gmail.com>
2022-09-01 22:38:59 -0300

    aatt

--- cadastro.js
+++ cadastro.js
@@ -1,15 +1,15 @@
 const form = document.querySelector("#aform_cadastro");
 
-form.addEventListener('submit', (event))=> {
-    
-    event.preventDefault();
+//form.addEventListener('submit', (event))=> {
 
-    let novoCadastro = {
-        nome : form.nome.value,
-        email : form.email.value,
-        tel : form.tel.value,
-        sex : form.sex.value,
-        aniversario : form.aniversario.value
-    }
-    console.log(novoCadastro)
-}
+ //   event.preventDefault();
+
+  //  let novoCadastro = {
+   //     nome : form.nome.value,
+  //      email : form.email.value,
+  //      tel : form.tel.value,
+ //       sex : form.sex.value,
+  //      aniversario : form.aniversario.value
+  //  }
+   // console.log(novoCadastro)
+//}
