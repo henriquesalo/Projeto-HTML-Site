6bcf6e2f86cd3c8a9ba9b386ba635bee4cadb65e
Eduardo Antero de Moraes <96095997+duduantero@users.noreply.github.com>
2022-09-01 22:53:50 -0300

    att

--- cadastro.js
+++ cadastro.js
@@ -8,7 +8,6 @@ form.addEventListener('submit', (event)=> {
       nome : form.nome.value,
       email : form.email.value,
       tel : form.tel.value,
-      sex : form.sex.value,
       aniversario : form.aniversario.value
     }
     console.log(novoCadastro)
