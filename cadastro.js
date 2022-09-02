1d56716b6b2a2d99f1da0ae3beba7872ab02315b
Henrique Salomao <henriquersalomao@gmail.com>
2022-09-01 22:56:09 -0300

    att

--- cadastro.js
+++ cadastro.js
@@ -1,15 +1,15 @@
 const form = document.querySelector("#aform_cadastro");
 
-//form.addEventListener('submit', (event))=> {
+form.addEventListener('submit', (event))=> {
 
- //   event.preventDefault();
+    event.preventDefault();
 
-  //  let novoCadastro = {
-   //     nome : form.nome.value,
-  //      email : form.email.value,
-  //      tel : form.tel.value,
- //       sex : form.sex.value,
-  //      aniversario : form.aniversario.value
-  //  }
-   // console.log(novoCadastro)
-//}
+    let novoCadastro = {
+        nome : form.nome.value,
+        email : form.email.value,
+        tel : form.tel.value,
+        sex : form.sex.value,
+        aniversario : form.aniversario.value
+    }
+    console.log(novoCadastro)
+}
