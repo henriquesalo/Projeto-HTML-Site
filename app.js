fed1b6132fe711b29d6d8625b9c5953ed6af5466
Eduardo Antero de Moraes <96095997+duduantero@users.noreply.github.com>
2022-09-08 17:08:08 -0300

    att versão sem pushnotification

---
+++ app.js
@@ -0,0 +1,36 @@
+function cadastro(){
+    alert("Seus cadastro foi efetuado com sucesso !");
+}
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAsjcPpOoHtAH3ZtqR_P-YIONB4PbaS_I0",
+    authDomain: "diasdeoracao.firebaseapp.com",
+    projectId: "diasdeoracao",
+    storageBucket: "diasdeoracao.appspot.com",
+    messagingSenderId: "1067463051687",
+    appId: "1:1067463051687:web:35da583bf663268d5cce82"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+const form = document.querySelector('#form_cadastro');
+
+form.addEventListener('submit',(event)=>{
+event.preventDefault();
+
+let novoCadastro = {
+    nome: form.nome.value,
+    email: form.email.value,
+    tel: form.tel.value,
+    nascimento: form.nascimento.value
+}
+
+db.collection('Cadastros').add({
+   novoCadastro: novoCadastro
+})
+
+console.log(novoCadastro)
+
+})
